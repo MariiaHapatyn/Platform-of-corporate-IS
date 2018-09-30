b9e631b55427af6ae85f9686f74ab49bb96d1d9d
andrewDubyk <andrew.dubyk7@gmail.com>
2018-09-30 18:14:08 +0300

    add Hexagone class
    add const variable for vertices in hexagone

---
+++ Task2/Task2/Classes/Hexagone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Task2.Classes
+{
+    [Serializable]
+    public class Hexagone
+    {
+        private readonly int VERTICES_NUMBER = 6;
+        private PointCollection points;
+        public PointCollection Points
+        {
+            get
+            {
+                return points;
+            }
+            set
+            {
+                if (value.Count != VERTICES_NUMBER)
+                {
+                    throw new ArgumentException("Number of points must be 6");
+                }
+                points = value;
+            }
+        }
+        public Color HexagoneColor { get; set; }
+
+        public Hexagone() { }
+
+        public Hexagone(Polygon figure)
+        {
+            Points = figure.Points;
+            HexagoneColor = (figure.Fill as SolidColorBrush).Color;
+        }
+    }
+}
