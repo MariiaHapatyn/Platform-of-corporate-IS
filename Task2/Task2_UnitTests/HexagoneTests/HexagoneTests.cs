6269cef7ad7a0fe23211fe8d2481c791a36a853c
Olya <olyapastuschak@gmail.com>
2018-10-08 21:51:05 +0300

    added unit tests for Hexagone

---
+++ Task2/Task2_UnitTests/HexagoneTests/HexagoneTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task2.Classes;
+using System.Windows.Media;
+using System.Windows;
+
+
+namespace Task2_UnitTests
+{
+    [TestClass]
+    public class HexagoneTests
+    {
+        [TestMethod]
+        public void Test_PointsProperty()
+        {
+            Hexagone hexagone = new Hexagone();
+            PointCollection points = new PointCollection
+            {
+                new Point(67, 89),
+                new Point(67, 45),
+                new Point(56, 23),
+                new Point(10, 9),
+                new Point(100, 145),
+                new Point(23, 76)
+            };
+            hexagone.Points = points;
+            Assert.AreEqual(hexagone.Points, points);
+        }
+
+        [TestMethod]
+        public void Test_HexagoneColorProperty()
+        {
+            Hexagone hexagone = new Hexagone();
+            hexagone.HexagoneColor = Colors.Red;
+            Assert.AreEqual(hexagone.HexagoneColor, Colors.Red);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_PointsPropertyException()
+        {
+            Hexagone hexagone = new Hexagone();
+            PointCollection points = new PointCollection();
+            //Too many points for hexagone (7)
+            points.Add(new Point(67, 89));
+            points.Add(new Point(67, 45));
+            points.Add(new Point(56, 23));
+            points.Add(new Point(10, 9));
+            points.Add(new Point(100, 145));
+            points.Add(new Point(23, 76));
+            points.Add(new Point(23, 76));
+            hexagone.Points = points;
+        }
+    }
+}
