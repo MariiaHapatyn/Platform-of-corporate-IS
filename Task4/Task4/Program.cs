793abbcd00fdeb8a4808783787d9b454055a2052
andrewDubyk <andrew.dubyk7@gmail.com>
2018-11-18 10:48:45 +0200

    Add instnwnd.sql for Northwinddb, configure connection string, add basic queries for selecting items from db

--- Task4/Task4/Program.cs
+++ Task4/Task4/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,19 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            string connectionString = ConfigurationManager.ConnectionStrings["Task4.Properties.Settings.NorthwindConnectionString"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            Console.WriteLine("Show the list of first and last names of the employees from London");
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT FirstName, LastName FROM Employees WHERE City='London';";
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("{0}\t{1}", reader["FirstName"], reader["LastName"]);
+            }
+            reader.Close();
         }
     }
 }
